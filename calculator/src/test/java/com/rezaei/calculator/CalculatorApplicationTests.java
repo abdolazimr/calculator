c11790ee0a069e06bcc89e1c5e38a93031606c6c
abdolazimr <abdolazimr@gmail.com>
2024-01-24 10:35:01 +0100

    calculator project

---
+++ calculator/src/test/java/com/rezaei/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rezaei.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

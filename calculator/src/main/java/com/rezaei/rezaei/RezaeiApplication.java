b3d09ec009865c4f760b66c0b11675a65c9aa23e
abdolazimr <abdolazimr@gmail.com>
2024-01-24 20:15:52 +0100

    new version

---
+++ calculator/src/main/java/com/rezaei/rezaei/RezaeiApplication.java
@@ -0,0 +1,13 @@
+package com.rezaei.rezaei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezaeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezaeiApplication.class, args);
+	}
+
+}
---
+++ calculator/src/test/java/com/rezaei/rezaei/RezaeiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rezaei.rezaei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezaeiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
